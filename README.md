0a085d95da43f8a99b13b445379c813882ee1c11
1585685035@qq.com <1585685035@qq.com>
2020-12-03 21:32:02 +0800

    提交

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # loges-spring-boot-starter
 log日志前端显示
 https://blog.csdn.net/zhanghui3239619/article/details/78608662
+
+
+```xml
+<?xml version="1.0" encoding="UTF-8"?>
+<configuration scan="true">
+    <include resource="org/springframework/boot/logging/logback/defaults.xml"/>
+    <property name="LOG_FILE"
+              value="${LOG_FILE:-${LOG_PATH:-${LOG_TEMP:-${java.io.tmpdir:-/tmp}}/}spring.log}"/>
+    <include resource="org/springframework/boot/logging/logback/file-appender.xml"/>
+    <appender name="CONSOLE" class="ch.qos.logback.core.ConsoleAppender">
+        <encoder>
+            <pattern>${CONSOLE_LOG_PATTERN}</pattern>
+            <charset>utf8</charset>
+        </encoder>
+        <filter class="com.github.nntk.LogFilter"></filter>
+    </appender>
+    <root level="INFO">
+        <appender-ref ref="FILE"/>
+        <appender-ref ref="CONSOLE"/>
+    </root>
+</configuration>
+
+```
--- src/main/java/com/github/nntk/WebSocketConfig.java
+++ src/main/java/com/github/nntk/WebSocketConfig.java
@@ -1,15 +1,53 @@
 package com.github.nntk;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import javax.annotation.PostConstruct;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Configuration
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket")
-                .setAllowedOrigins("http://localhost:8976")
+        registry.addEndpoint("/logSocket")
+                .setAllowedOrigins("*")
                 .withSockJS();
     }
+
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+
+    @PostConstruct
+    public void pushLogger() {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+
+                    try {
+                        LoggerMessage log = LoggerQueue.getInstance().poll();
+                        if (log != null) {
+                            if (messagingTemplate != null)
+                                messagingTemplate.convertAndSend("/topic/pullLogger", log);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        executorService.submit(runnable);
+        executorService.submit(runnable);
+    }
+
 }
