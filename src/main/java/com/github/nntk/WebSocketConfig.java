5d8cf3e824328fd2ff78f3695d0ba0ad0c39358e
huanghaoming <1585685035@qq.com>
2020-12-04 14:22:40 +0800

    提交

--- src/main/java/com/github/nntk/WebSocketConfig.java
+++ src/main/java/com/github/nntk/WebSocketConfig.java
@@ -1,6 +1,8 @@
 package com.github.nntk;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -8,11 +10,15 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 import javax.annotation.PostConstruct;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@Slf4j
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -25,9 +31,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
 
+    @Value("${local.server.port:8080}")
+    private String port;
 
     @PostConstruct
     public void pushLogger() {
+
+        log.info("Welcome use loges,you can visit this url to open web quickly: " + getUrl());
+
+
         ExecutorService executorService = Executors.newFixedThreadPool(2);
         Runnable runnable = new Runnable() {
             @Override
@@ -50,4 +62,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         executorService.submit(runnable);
     }
 
+
+    public String getUrl() {
+        InetAddress localHost = null;
+        try {
+            localHost = Inet4Address.getLocalHost();
+        } catch (UnknownHostException e) {
+        }
+        String ip = localHost.getHostAddress();
+
+        return "http://" + ip + ":" + port + "/loges/index.html";
+    }
 }
