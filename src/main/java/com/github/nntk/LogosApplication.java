1d28e49b4af5f310d6af43fe4b54d9f28fb6b2f6
1585685035@qq.com <1585685035@qq.com>
2020-12-19 10:24:55 +0800

    提交

--- src/main/java/com/github/nntk/LogosApplication.java
+++ src/main/java/com/github/nntk/LogosApplication.java
@@ -10,15 +10,15 @@ public class LogosApplication {
     public static void main(String[] args) {
         SpringApplication.run(LogosApplication.class, args);
 
-//        while (true) {
-//            log.info("_____");
-//            log.warn("_____");
-//            log.error("_____");
-//            try {
-//                Thread.sleep(500);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
+        while (true) {
+            log.info("_____");
+            log.warn("_____");
+            log.error("_____");
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
